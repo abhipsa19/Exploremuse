f0c363dfa8e198564dc2ab5179c98534d546052b
abhipsa19 <abhipsasahoo2003@gmail.com>
2024-10-24 13:29:31 +0530

    login after signup

--- routes/user.js
+++ routes/user.js
@@ -14,8 +14,14 @@ router.post("/signup", wrapAsync(async (req, res) => {
     const newUser = new User({ email, username });
     const registeredUser = await User.register(newUser, password);
     console.log(registeredUser);
-    req.flash("success", "Welcome to Exploremuse!");
-    res.redirect("/listings");
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome to Exploremuse!");
+      res.redirect("/listings");
+    });
+
 
   } catch (e) {
     req.flash("error", e.message);
