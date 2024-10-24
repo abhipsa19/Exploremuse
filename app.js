8aeb10a81be68573ea6ada9792095343c00c1809
abhipsa19 <abhipsasahoo2003@gmail.com>
2024-10-24 10:48:27 +0530

    setting user model,signup and loginn of user

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require('connect-flash');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Exploremuse";
@@ -55,6 +60,12 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
@@ -65,8 +76,11 @@ app.use((req, res, next) => {
 
 
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 // app.get("/testListing", async (req, res) => {
